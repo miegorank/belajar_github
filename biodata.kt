0f67ef7b853040647f07d39487dc7150d14baf08
miegorank <145540191+miegorank@users.noreply.github.com>
2023-09-20 15:06:57 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var name = "Muhammad Naufal Nurfikri"
+  var kelas = "XI RPL 2"
+  var hobi = "playing football"
+
+  println("Nama: $nama")
+  println("Kelas: $kelas")
+  println("Nama: $hobi")
+}
